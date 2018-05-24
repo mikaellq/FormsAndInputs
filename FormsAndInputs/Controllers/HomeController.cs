4642c0f7012a1ffd5501bfd73d570757c7c39676
mikaellq <38102084+mikaellq@users.noreply.github.com>
2018-05-24 11:04:22 +0200

    Last for thursday
    Added radio buttons, conversion of fahrenheit to celcius and even a default state in the radio buttons using MShtml helper.

---
+++ FormsAndInputs/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using FormsAndInputs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormsAndInputs.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return Redirect("/Home/CheckNumber");
+        }
+        
+        public ActionResult CheckNumber()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CheckNumber(string input, string unit)
+        {
+            ViewBag.Result = Temperature.FeberTest(input, unit);
+
+            return View();
+        }
+    }
+}
---
+++ FormsAndInputs/Models/Temperature.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FormsAndInputs.Models
+{
+    public static class Temperature
+    {
+        public static string FeberTest(string input, string unit)
+        {
+            string s = "";
+            float number = 0;
+
+            if (!float.TryParse(input, out number))
+            {
+                return "Vänligen skriv in din temperatur!";
+            }
+
+            if (unit == "Fahrenheit")
+                number = (number - 32) / 1.8f;
+
+            if (number >= 40)
+                s = "Du har hög feber";
+            else if (number >= 38)
+                s = "Du har feber";
+            else if (number >= 36)
+                s = "Du är feberfri";
+            else
+                s = "Du är nedkyld";
+
+            return s;
+        }
+    }
+}
